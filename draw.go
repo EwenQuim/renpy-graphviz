246641252785013cc2cf1c218378458b19e597bb
EwenQuim <ewen.quimerch@gmail.com>
2021-03-06 12:46:15 +0100

    Supports call & return

--- draw.go
+++ draw.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 
 	"github.com/goccy/go-graphviz"
 )
 
 func (g RenpyGraph) makeGraph() {
+	fmt.Println("Drawing the renpy-graphviz.png file...")
 
 	if err := g.graphviz.RenderFilename(g.graph, graphviz.PNG, "renpy-graphviz.png"); err != nil {
 		log.Fatal(err)
@@ -17,6 +19,8 @@ func (g RenpyGraph) makeGraph() {
 	if err := g.graphviz.Render(g.graph, "dot", &buf); err != nil {
 		log.Fatal(err)
 	}
+	writeFile("renpy-graphviz.dot", buf.String())
+
 	// fmt.Println(buf.String())
 
 }
