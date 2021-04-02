1d861f69d0380ac9620799a2fce6cb54bb976e4c
EwenQuim <ewen.quimerch@gmail.com>
2021-04-02 19:59:20 +0200

    consider as useful

--- parser/utils.go
+++ parser/utils.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -15,4 +16,21 @@ func RunningTime(s string) (string, time.Time) {
 func Track(s string, startTime time.Time) {
 	endTime := time.Now()
 	fmt.Println("  - ", endTime.Sub(startTime))
+}
+
+// We ConsiderAsUseful a file when it is not a translation file,
+// or isn't options/gui/screens .rpy
+func ConsiderAsUseful(fullPath string) bool {
+	switch {
+	case strings.Contains(fullPath, "tl/"):
+		return false
+	case strings.Contains(fullPath, "options.rpy"):
+		return false
+	case strings.Contains(fullPath, "screens.rpy"):
+		return false
+	case strings.Contains(fullPath, "gui.rpy"):
+		return false
+	default:
+		return true
+	}
 }
