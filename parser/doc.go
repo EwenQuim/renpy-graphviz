0dcbde0c333fd5865811df3ec6ee9be19f3fa497
EwenQuim <ewen.quimerch@gmail.com>
2021-04-23 16:13:38 +0200

    added a simple test case

--- parser/doc.go
+++ parser/doc.go
@@ -1,4 +1,4 @@
-// TPackage parser helps understand the structure
+// Package parser helps understand the structure
 // of a ren'py game by drawing a dot graph of its structure
 // see https://graphviz.org/doc/info/lang.html
 
