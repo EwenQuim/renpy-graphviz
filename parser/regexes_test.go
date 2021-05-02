ea03b40f9055adeadfecdffc92e1ab53d44fb577
EwenQuim <ewen.quimerch@gmail.com>
2021-05-02 15:47:25 +0200

    Detecting choices

--- parser/regexes_test.go
+++ parser/regexes_test.go
@@ -61,3 +61,88 @@ func TestMenuStatement(t *testing.T) {
 		})
 	}
 }
+
+func TestReturnStatement(t *testing.T) {
+	t.Parallel()
+	detect := initializeDetectors()
+
+	testCases := []struct {
+		id             int
+		line           string
+		updatedContext bool
+	}{
+		{0, "    return", true},
+		{1, "return", true},
+		{2, "    return # comments", true},
+		{3, "        return # comments", false},
+		{4, "	eileen \"I want to return:\" ", false},
+		{5, "	 # comments return", false},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Running test %v", tc.id), func(t *testing.T) {
+
+			if detect.returns.MatchString(tc.line) != tc.updatedContext {
+				t.Errorf("Error in test %v", tc.id)
+			}
+		})
+	}
+}
+
+func TestChoiceRegex(t *testing.T) {
+	t.Parallel()
+	detect := initializeDetectors()
+
+	testCases := []struct {
+		id             int
+		line           string
+		updatedContext bool
+		choice         string
+	}{
+		{0, "    \"choice one\":", true, "choice one"},
+		{1, "	'choice one':", true, "choice one"},
+		{2, "    \"Ne pas parler\" (\"default\", 8.0):", true, "Ne pas parler"},
+		{3, "        \"choice one\": # comments", true, "choice one"},
+		{4, "	j \"Exact.\" ", false, ""},
+		{5, "	 \"exact\" # comments return", false, ""},
+		{6, "	\"It's a videogame.\":", true, "It's a videogame."},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Running test %v", tc.id), func(t *testing.T) {
+
+			if detect.choice.MatchString(tc.line) != tc.updatedContext {
+				t.Errorf("Error in test %v", tc.id)
+			} else if tc.updatedContext {
+				if choice := detect.getChoice(tc.line); choice != tc.choice {
+					// t.Logf("%+v", sub)
+					t.Errorf("Error in test %v, %v != %v", tc.id, choice, tc.choice)
+				}
+			}
+		})
+	}
+}
+
+func TestGetIndent(t *testing.T) {
+	detect := initializeDetectors()
+	t.Parallel()
+
+	testCases := []struct {
+		line    string
+		indents int
+	}{
+		{"jump truc", 0},
+		{"    jump truc", 4},
+		{"        jump truc", 8},
+		{"      jump truc", 6},
+		{" jump truc", 1},
+	}
+
+	for _, tc := range testCases {
+		t.Run("Detect indentation ", func(t *testing.T) {
+			indent := detect.getIndent(tc.line)
+
+			if indent != tc.indents {
+				t.Errorf("Error in indent test:\n got %+v\nwant %+v", indent, tc.indents)
+			}
+		})
+	}
+}
